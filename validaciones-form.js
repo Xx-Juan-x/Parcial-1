17a297c6604acd999524cba93aac9ed95de74530
JuanIgnacioAmarilloJacob <amarillojuanignacio8@gmail.com>
2022-06-04 20:54:48 -0300

    Validacion edad

--- validaciones-form.js
+++ validaciones-form.js
@@ -26,6 +26,23 @@ else{
     document.querySelector("form #apellido").classList.remove("error");
 }
 
+if(form.querySelector("#edad").value == ""){
+    errorFormulario = true;
+    document.querySelector("form .conteiner-edad .text-error").innerHTML = "Debe completar la edad";
+    document.querySelector("form #edad").classList.add("error");
+}
+else if(form.querySelector("#edad").value < 0 || form.querySelector("#edad").value > 100){
+    errorFormulario = true;
+    document.querySelector("form .conteiner-edad .text-error").innerHTML = "La edad debe ser entre 0 - 100";
+    document.querySelector("form #edad").classList.add("error");
+}
+else{
+    document.querySelector("form .conteiner-edad .text-error").innerHTML = "";
+    document.querySelector("form #edad").classList.remove("error");
+}
+
+
+
 if(errorFormulario == false){
     form.submit();
 }
