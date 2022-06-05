3352db4e8b302c79b94a38fabab952ad00ea7982
JuanIgnacioAmarilloJacob <amarillojuanignacio8@gmail.com>
2022-06-04 21:30:15 -0300

    Validacion Email

--- validaciones-form.js
+++ validaciones-form.js
@@ -5,27 +5,49 @@ form.addEventListener("submit",function(Event){
 Event.preventDefault();
 
 var errorFormulario = false;
+let mensaje_nombre = document.querySelector(".conteiner-nombre .text-error");
+let nombre = document.querySelector("#nombre");
 
-if(form.querySelector("#nombre").value.length < 3){
+//Validacion nombre
+if(nombre.value.length < 3){
     errorFormulario = true;
-    document.querySelector("form .conteiner-nombre .text-error").innerHTML = "El nombre debe contener 3 o más caracteres";
-    document.querySelector("form #nombre").classList.add("error");
+    mensaje_nombre.innerHTML = "El nombre debe contener 3 o más caracteres";
+    nombre.classList.add("error");
 }
 else{
-    document.querySelector("form .conteiner-nombre .text-error").innerHTML = "";
-    document.querySelector("form #nombre").classList.remove("error");
+    mensaje_nombre.innerHTML = "";
+    nombre.classList.remove("error");
 }
 
+//Validacion apellido
 if(form.querySelector("#apellido").value.length < 3){
     errorFormulario = true;
-    document.querySelector("form .conteiner-apellido .text-error").innerHTML = "El apellido debe contener 3 o más caracteres";
-    document.querySelector("form #apellido").classList.add("error");
+    document.querySelector(".conteiner-apellido .text-error").innerHTML = "El apellido debe contener 3 o más caracteres";
+    document.querySelector("#apellido").classList.add("error");
 }
 else{
-    document.querySelector("form .conteiner-apellido .text-error").innerHTML = "";
-    document.querySelector("form #apellido").classList.remove("error");
+    document.querySelector(".conteiner-apellido .text-error").innerHTML = "";
+    document.querySelector("#apellido").classList.remove("error");
 }
 
+//Validacion Email
+var regex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+if(!regex.test(document.querySelector("#email").value)){
+    errorFormulario = true;
+    document.querySelector(".conteiner-email .text-error").innerHTML = "El formato del email es incorrecto";
+    document.querySelector("#email").classList.add("error");
+}else{
+    document.querySelector(".conteiner-email .text-error").innerHTML = "";
+    document.querySelector("#email").classList.remove("error");
+}
+
+
+
+
+
+
+
+//validacion edad
 if(form.querySelector("#edad").value == ""){
     errorFormulario = true;
     document.querySelector("form .conteiner-edad .text-error").innerHTML = "Debe completar la edad";
